7aea87026c4a616fedc935fbc7607d8bfdd3ca1d
Mason Proffitt <mason.louis.proffitt@cern.ch>
2017-08-06 07:32:01 +0200

    Added c-tagging ROC curve

---
+++ makeroccurves.py
@@ -0,0 +1,15 @@
+import h5py
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+
+f = h5py.File('output.h5',  'r')
+cfpr, ctpr, _ = roc_curve(f['jet']['particle_id'], f['jet']['rnnip_pc'], 4)
+bfpr, btpr, _ = roc_curve(f['jet']['particle_id'], f['jet']['rnnip_pb'], 5)
+f.close()
+plt.title('ROC curves for RNNIP flavour tagging')
+plt.ylabel('Background rejection')
+plt.xlabel('Signal efficiency')
+plt.plot(ctpr, 1 - cfpr, label='c jets')
+plt.plot(btpr, 1 - bfpr, label='b jets')
+plt.legend(loc='lower left')
+plt.savefig('roccurves.png')
