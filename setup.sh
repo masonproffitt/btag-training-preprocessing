5264061e33bafc5b38d905fc7e25aaedb7633939
Dan Guest <dguest@cern.ch>
2017-08-08 15:22:00 +0200

    Add some comments pointing to JIRA tickets

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ asetup AnalysisBase,21.2.0
 
 # setup HDF5
 echo "=== setting up HDF5 ==="
-lsetup hdf5
-
+# This next line is commented out until we figure out
+#
+# https://sft.its.cern.ch/jira/browse/SPI-984
+#
+# Until then we need to use some weird flags building the hdf5
+# wrappers
+#
+# ------ uncomment when fixed -----
+# lsetup "hdf5 1.10.0-x86_64-slc6-gcc62-opt"
+# ---------------------------------
+lsetup hdf5                     # <-- and then remove this
 
